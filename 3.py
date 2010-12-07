a1f77a09992ad7c3a31d305365844a10ef3fa00b
Artur Czajka <arturczajka@gmail.com>
2010-12-07 15:49:02 +0100

    Zadanie #3
    Ka?da instancja klasy Bezier to cz?onek populacji.
    Tablice instancji b?d? populacjami.

---
+++ 3.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import sys
+import numpy as np
+import math as m
+import random as rand
+import matplotlib.pyplot as plot
+
+class Bezier:
+    def __init__(self, height, width):
+        self.begin = (0, height)
+        self.end = (width, 0)
+        self.p0 = [2 * rand.random() * width - width, 2 * rand.random() * height - height]
+        self.p1 = [2 * rand.random() * width - width, 2 * rand.random() * height - height]
+
+    def mutate(self):
+        pass
+
+    def mate(self, other):
+        pass
+
+    def _at(self, x):
+        """x = [0, 1]"""
+        return (1-x)**3 * self.begin + 3 * (1-x)**2 * x * self.p0 \
+            + 3 * (1-x) * x**2 * self.p1 + x**3 * self.end
+
+    def time(self, g = 9.81):
+        """t = l / sqrt(2 * g * h)"""
+        pass
+
+if __name__ == "__main__":
+    b = Bezier(5, 10)
+    print "Hello world!"
