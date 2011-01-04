75b2b6718e351753d6eb96a7dac93f1b1843e521
Artur Czajka <arturczajka@gmail.com>
2011-01-04 14:36:44 +0100

    Pierd??ka. Powoli prace czas zacz?? ;)

--- 4/z4.py
+++ 4/z4.py
@@ -1,5 +1,20 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+import sys
+import numpy as np
+import math as m
+import random as r
+import getopt as go
+
+import matplotlib.pylot as plt
+
 if __name__ == "__main__":
-    pass
+    demoMode = False;
+
+    opts, args = go.getopt(sys.argv[1:], "D")
+    for opt, arg in opts:
+        if opt == "-D":
+            demoMode = True
+
+    print "Hello world!"
