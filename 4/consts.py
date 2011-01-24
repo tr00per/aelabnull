d44ca5b3057a69ba9a3e2fadbda08196518c195a
Cezar "ikari" Pokorski <commited.by@ikari.pl>
2011-01-24 12:33:26 +0100

    Krzyzowanie, mutacja, cos tam.

--- 4/consts.py
+++ 4/consts.py
@@ -1,10 +1,7 @@
 # -*- coding: utf-8 -*-
 
-#legal_one_filter = ("__doc__", "__name__", "__package__", "atan2", "copysign", "e", "fmod", "frexp", "fsum", "hypot", "isinf", "isnan", "ldexp", "modf", "pi", "pow")
-#legal_one = tuple( [elem for elem in dir(math) if elem not in legal_one_filter] )
-
-legal_one = ('acos', 'acosh', 'asin', 'asinh', 'atan', 'atanh', 'cos', 'cosh', 'exp', 'log', 'log10', 'sin', 'sinh', 'sqrt', 'tan', 'tanh')
-legal_two = ('+', '-', '*', '/', '//', '**')
+legal_one = ('acos', 'acosh', 'asin', 'asinh', 'atan', 'atanh', 'cos', 'cosh', 'exp', 'log', 'log10', 'sin', 'sinh', 'sqrt', 'tan', 'tanh', 'cos', 'sin', 'log', 'log10', 'sqrt', 'cos', 'sin')
+legal_two = ('+', '-', '*', '/', '**')
 legal_op = legal_two + legal_one
 legal_other = ("x", "e", "pi")
 legal_all = legal_other + legal_op
--- 4/population.py
+++ 4/population.py
@@ -10,12 +10,12 @@ class Population:
         self.TO = x_to
         self.STEP = np.abs(x_to - x_from) * 0.05 #21 control points
         self.TARGET = self.__calc_values(target)
-
+        self.N = n
         self.POP = [self.random_individual() for i in range(n)]
 
     def __calc_values(self, specimen):
         ret = []
-        for x in np.arange(self.FROM, self.TO+self.STEP, self.STEP):
+        for x in np.arange(self.FROM, self.TO + self.STEP, self.STEP):
             try:
                 ret.append(specimen.calc(x))
             except ZeroDivisionError:
@@ -24,18 +24,55 @@ class Population:
 
     def __calc_diffs(self):
         #FIXME calc differentials
-        return [ self.__calc_values(specimen) for specimen in self.POP ]
+        delta = 0.001
+        dx = 2 * delta
+        diffs = []
+        for specimen in self.POP:
+            values = []
+            for x in np.arange(self.FROM, self.TO + self.STEP, self.STEP):
+                dy = specimen.calc(x + delta) - speciment.calc(x - delta)
+                #dx = 2 * delta
+                # i feel a strange urge to use numpy somehwere here
+                values.append( dy/dx )
+            diffs.append(values)
+        return diffs
+        # oh fck it.
+        # return [ self.__calc_values(specimen) for specimen in self.POP ]
 
     def epoch(self):
+        # some parameters:
+        p_crossing = 0.8
+        p_mutation = 0.1
+
         newPop = []
         diffs = self.__calc_diffs
 
         for i in range(len(diffs)):
-            diffs[i] = np.abs(diffs[i]).sum()
+            diffs[i] = np.abs(diffs[i]-self.TARGET).sum()
 
         best = np.array(diffs).argsort()
 
-        #FIXME now what...?
+        parents = []
+        idx = 0
+        while len(newPop) < self.N:
+            if r.random() < p_crossing:
+                parents.append(self.POP[best.pop(idx)])
+                idx -= 1 # will increase back to current value...
+                if len(parents) == 2:
+                    # BREED!
+                    children = parents[0].copulate(parents[1])
+                    newPop += children
+                    parents = []
+            else:
+                # maybe mutate if not breeding?
+                if r.random() < p_mutation:
+                    self.POP[best[idx]].mutate()
+            idx += 1
+            if idx > len(best):
+                idx = 0
+
+        self.POP = newPop
+
 
     def random_individual(self):
         return Node.random_node(0)
